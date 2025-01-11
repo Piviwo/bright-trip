2cd546afcf2d09adee993edfeab492f5c6b7e15e
Tanya <t.pozdnyakova@tum.de>
2025-01-11 14:12:31 +0100

    adding a list to Support page

--- app/src/main/java/com/pia/brighttrip/SupportFragment.java
+++ app/src/main/java/com/pia/brighttrip/SupportFragment.java
@@ -7,6 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,7 +62,27 @@ public class SupportFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_support, container, false);
+        View view = inflater.inflate(R.layout.fragment_support, container, false);
+
+        // Referencing the ListView in the fragment layout
+        ListView listView = view.findViewById(R.id.call_list);
+
+        // List of emergency contacts
+        ArrayList<String> contactNames = new ArrayList<>();
+        contactNames.add("Police: 110");
+        contactNames.add("Women's Helpline: (030) 610063");
+        contactNames.add("Mental Health Helpline: (030) 3906300");
+
+        // Setting up an ArrayAdapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                requireContext(),
+                android.R.layout.simple_list_item_1,
+                contactNames
+        );
+
+        listView.setAdapter(adapter);
+
+        return view;
     }
+
 }
