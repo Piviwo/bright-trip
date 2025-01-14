65ae431e07edc2cf7919cb736945f333c3f51287
Tanya <t.pozdnyakova@tum.de>
2025-01-14 19:05:37 +0100

    added intents to call the emergency numbers (need to test if it actually works)

--- app/src/main/java/com/pia/brighttrip/SupportFragment.java
+++ app/src/main/java/com/pia/brighttrip/SupportFragment.java
@@ -3,13 +3,13 @@ package com.pia.brighttrip;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
+import android.widget.Toast;
 import java.util.ArrayList;
 
 public class SupportFragment extends Fragment {
@@ -28,20 +28,42 @@ public class SupportFragment extends Fragment {
         ListView listView = view.findViewById(R.id.call_list);
 
         //todo: add intents
-        // List of emergency contacts
-        ArrayList<String> contactNames = new ArrayList<>();
-        contactNames.add("Police: 110");
-        contactNames.add("Women's Helpline: (030) 610063");
-        contactNames.add("Mental Health Helpline: (030) 3906300");
+        // List of emergency contacts and call numbers
+        final ArrayList<String[]> contactDetails = new ArrayList<>();
+        contactDetails.add(new String[]{"Police", "110"});
+        contactDetails.add(new String[]{"Women's Helpline", "(030)610063"});
+        contactDetails.add(new String[]{"Mental Health Helpline", "(030)3906300"});
 
         // Setting up an ArrayAdapter
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                requireContext(),
-                android.R.layout.simple_list_item_1,
-                contactNames
-        );
+        ArrayAdapter<String[]> adapter = new ArrayAdapter<String[]>(requireContext(), android.R.layout.simple_list_item_1, contactDetails) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                String[] contact = contactDetails.get(position);
+                // displaying the contact and phone number in the list item
+                ((TextView) view.findViewById(android.R.id.text1)).setText(contact[0] + ": " + contact[1]);
+                return view;
+            }
+        };
 
         listView.setAdapter(adapter);
+
+        //setting up OnItemClickListener
+        listView.setOnItemClickListener((parent, view1, position, id) -> {
+            String[] contact = contactDetails.get(position);
+            String phoneNumber = contact[1];
+
+            //intent to call the number
+            Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+            dialIntent.setData(Uri.parse("tel:" + phoneNumber));
+
+            //dial activity
+            try {
+                startActivity(dialIntent); // Start the dial activity
+            } catch (Exception e) {
+                Toast.makeText(requireContext(), "Failed to open dialer.", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return view;
     }
